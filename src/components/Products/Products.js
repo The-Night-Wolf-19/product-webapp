fc53cc434dc68484182de1c8ea27310dd266ff4c
Shubham Sinha <shubhamsinha.cer18@itbhu.ac.in>
2022-02-15 04:46:00 +0530

    minor design change

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -13,7 +13,7 @@ export const Products = ({ products, setSelected, selected }) => {
       <p className="text-lg">{products[0].product_name}</p>
       <hr className="mb-2" />
       <div className="w-inherit p-3 flex flex-row overflow-hidden bg-secondary rounded-[1.1rem]">
-        <div className="productDiv w-inherit min-w-full flex flex-row overflow-auto overflow-y-hidden scrollbar-thin scrollbar-thumb-slate-300 ">
+        <div className=" p-3 w-inherit min-w-full flex flex-row overflow-auto overflow-y-hidden scrollbar-thin scrollbar-thumb-slate-300 ">
           {elements}
         </div>
       </div>
